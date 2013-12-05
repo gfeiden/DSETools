852654a03196bab60e36acdf4cb75051b75b688d
gfeiden <gregory.a.feiden.gr@dartmouth.edu>
2013-12-05 16:35:51 +0100

    Changed init format

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ __credits__    = []
 
 __license__    = 'Beerware (version 42)'
 __version__    = '1.0.0'
-__data__       = '2013 Nov 28'
+__date__       = '2013 Nov 28'
 __maintainer__ = 'Gregory Feiden'
 __email__      = 'gregory.a.feiden.gr@dartmouth.edu'
 __status__     = 'Development'
